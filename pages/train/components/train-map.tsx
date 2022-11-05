e0087e86a43a5c3d086b30b9d27e42abda20096a
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2022-11-04 22:04:33 -0400

    consolidate mbta fetch utility functions

--- pages/train/components/train-map.tsx
+++ pages/train/components/train-map.tsx
@@ -2,122 +2,81 @@ import '../../../styles/Train.module.css';
 import MBTA from 'mbta-client';
 import {useEffect, useState, useRef, useImperativeHandle, useInsertionEffect} from 'react';
 import NextApp from 'next';
-import React, { ReactNode } from "react";
-import JSXStyle from 'styled-jsx/style';
-import { createSecureContext } from 'tls';
 import LinkedList from '../utils/linked-list';
 import { Area , Position , ArrivalData } from '../utils/types';
 import { callbackify } from 'util';
+import { fetchStopData } from '../utils/mbta-fetch';
+import { sliceAtFirstMutual } from '../utils/operations';
+import React from 'react';
 
-const mbta = new MBTA('7226f7ee55514c019819bac6eacdaad9');
-
+/* I made a variable with a global scope so that once the color prop is passed within TrainMap, 
+it can be accessed by other separate components */
 var staticColor:string = "#000000";
 
+// Global variable stores all stops grid positions
+// This is useful for adding event listeners to grid segments
 var coords = {};
 
-  
-async function getNextTrainTime(stop_id, direction_id) {
-    const handleResponse = res => {
-        // filter for upcoming train times that have a valid timestamp
-        var times = res.data.map(d => {
-            if (d.attributes.arrival_time != null) {
-                var timestamp = new Date(d.attributes.arrival_time);
-
-                if (timestamp.getTime() > Date.now()) {
-                    return timestamp
-                }
-            }
-        }).filter(d => d != null);
-
-        // find the soonest arriving train predicted to be arriving
-        var nearestArrival = null;
-        for (var timestamp of times) {
-            if (nearestArrival != null) {
-                nearestArrival = compareDates(timestamp, nearestArrival);
-            } else {
-                nearestArrival = timestamp;
-            }
-        }
-
-        // returns the soonest arriving train timestamp (returns 'null' if station unavailable)
-        return nearestArrival
-    }
-
-    return await mbta.fetchPredictions({stop: stop_id, sort:"arrival_time", limit:10, direction_id:direction_id}).then(res => handleResponse(res));
-
-}
-  
-function compareDates(a, b) {
-    // compares two Date objects and returns the lowest of the two
-    if (a.getTime() > b.getTime()) {
-        return b
-    } else if (a.getTime() < b.getTime()) {
-        return a
-    } else {
-        return b
-    }
-}
-
-
-function sliceAtFirstMutual(a: string[], b: string[]) {
-    // Accepts two arrays of strings as inputs
-    // Returns spliced segment of Array B starting from first shared value with Array A
-    var index: number = 0;
-    let indices = b.map(i => {
-        return a.lastIndexOf(i)
-    })
-    index = b.indexOf(a[Math.max(...indices)]);
-    return b.slice(index)
-    
-    
-}
-
-
+// LinkedTrain objects require that node values are of type string[]
+// So I made some extra methods for computing these node values
 class LinkedTrain extends LinkedList {
     offset: number = 0;
 
     constructor() {
+        // Inherit this.head
         super();
+        // Extend constructor
         this.offset = 0;
     }
 
+    // Locate where a node's value and the next node's value meet
+    /* If the node is the last in the linked list, return the distance, 
+    in grid number of grid cells, from the ending edge of the grid */
     findJunction(index: number) {
         let current = this.valueAt(index);
-        let parent = current.value;
-        let child = current.next ? current.next.value : null;
+        if (current != null) {
+            let parent = current.value;
+            let child = current.next ? current.next.value : null;
 
-        if (child != null) {
-            return parent.indexOf(child[0]);
-        } else {
-            return parent.length;
+            if (child != null) {
+                return parent.indexOf(child[0]);
+            } else {
+                return parent.length;
+            }
         }
-        
-        console.log(parent, child);
+        return null
     }
 
+    /* Calculate and return a node's total distance, in number of grid cells, 
+    from the starting edge of the grid */
     findDistance(index: number) {
         let current = this.valueAt(index);
-        let parent = current.value;
-        let child = current.next ? current.next.value : null;
+        if (current != null) {
+            let parent: string[] = current.value;
+            let child: string[] | null = current.next ? current.next.value : null;
 
-        if (child != null) {
-            return parent.indexOf(child[0]);
-        } else {
-            return 0;
+            if (child != null) {
+                return parent.indexOf(child[0]);
+            } else {
+                return 0;
+            }
         }
     }
 
 }
 
-
-function buildBranch(branch: Array<string>, x, y, color: string) {
-
+// Add all the stops for specified node into subgrid
+// Subgrids in CSS allow a grid to inherit the cell dimensions of its parent, no matter how many cells it contains
+// SUBGRID BE AWESOME
+function _buildBranch(branch: Array<string>, x, y, color: string) {
+    // Assign an array of all DOM representations of the branch's stops
     const l = branch.map((e: any, i: number) => {
-        coords[[x+i, y]] = e;
+        // Record all the filled grid cells so event listeners can be added
+        coords[[x+i, y]] = e; // YOU DIDN'T SEE ANYTHING, FOOL!
         
         return (
             <div style={{backgroundColor: color}} key={i} className='h-[20px] w-full font-PublicSans text-sm flex items-center justify-center'>
-                <div className='w-4 h-4 bg-white rounded-full'></div>
+                <div className='w-[1vw] h-[1vw] max-w-[14px] max-h-[14px] min-w-[10px] min-h-[10px] bg-white rounded-full'></div>
             </div>
         )
     });
@@ -136,21 +95,35 @@ function Stop({name, selectedRef, row, col}:{name: string, selectedRef: any[], r
 
 
 function Tooltip({ position , arrivalTimes } : { position: Position, arrivalTimes: ArrivalData }) {
+    const formatTime = (date: Date | null) => {
+        if (date == null) {
+            return "Unavailable"
+        }
+        let timeLeft = Math.floor((date.getTime() - Date.now()) / 60000);
+        switch (timeLeft) {
+            case 0:
+                return "Arriving now"
+            case 1:
+                return "Arriving soon"
+            default:
+                return timeLeft + " mins"
+        }
+    }
+
     return (
         <div style={{gridColumn: position.x, gridRow: position.y, borderColor: staticColor}} className='w-[150px] h-[150px] mt-8 bg-white border-[1px] transition-all p-2 justify-self-center'>
             <h1 style={{color: staticColor}} className='font-Inter text-md bold'>{arrivalTimes.name}</h1>
-            <p className='font-Inter text-md text-gray-300'>{Math.floor((arrivalTimes.left?.getTime() - Date.now()) / 60000) + "mins"}</p>
-            <p className='font-Inter text-md text-gray-300'>{Math.floor((arrivalTimes.right?.getTime() - Date.now()) / 60000) + "mins"}</p>
+            <p className='font-Inter text-md text-gray-300'>{formatTime(arrivalTimes.left)}</p>
+            <p className='font-Inter text-md text-gray-300'>{formatTime(arrivalTimes.right)}</p>
         </div>
     )
 }
 
 
-function RootBranch({children, rootList , trains , branches , d0, d1 }: {children: JSX.Element[] | JSX.Element, rootList: JSX.Element[], trains: LinkedTrain, branches: Array<string>[], d0: string, d1: string}) {
+function RootBranch({children, rootList , trains , branches , branchNames=[] }: {children: JSX.Element[] | JSX.Element, rootList: JSX.Element[], trains: LinkedTrain, branches: string[][], branchNames: string[]}) {
     const [overflow, setOverflow] = useState(0);
     const [selectedRef, setSelectedRef] = useState<Position | null>(null);
     const [toolTip, setToolTip] = useState<JSX.Element | null>(null);
-    const [arrivalTimes, setArrivalTimes] = useState<ArrivalData | null>(null);
 
     useEffect(() => computeBounds(), [children])
 
@@ -185,29 +158,19 @@ function RootBranch({children, rootList , trains , branches , d0, d1 }: {childre
 
     useEffect(() => {
         if (selectedRef != null) {
-            getNextTrainTime(selectedRef.name, 0).then(responseA => {
-                getNextTrainTime(selectedRef.name, 1).then(responseB => {
-                    mbta.fetchStops({id:selectedRef.name}).then((responseC: any) => {
-                        let arrivalTimes: ArrivalData = {
-                            right: responseA,
-                            left: responseB,
-                            name: responseC.data[0].attributes.name
-                        }
-                        setToolTip(<Tooltip  position={selectedRef} arrivalTimes={arrivalTimes} />);
-                    })
-                })
-            });
-            
-            
+            fetchStopData(selectedRef).then(time => {
+                setToolTip(<Tooltip  position={selectedRef} arrivalTimes={time} />);
+            })
         } else {
             setToolTip(null);
         }
     }, [selectedRef])
 
     return (
-        <div style={{gridTemplateColumns: `repeat(${rootList.length + overflow}, 1fr)`}} className='relative w-full grid gap-y-3'>
+        <div style={{gridTemplateColumns: `repeat(${rootList.length + overflow}, 1fr)`}} className='relative flex-grow grid gap-y-3'>
             <div style={{gridTemplateColumns: 'subgrid', gridColumn: '1/'+(rootList.length+1)}} className='grid h-auto rounded-full overflow-hidden rounded-full shadow-md'>{rootList}</div>
             {children}
+            
             
             <div style={{gridTemplateColumns: 'subgrid', gridTemplateRows: `repeat(${React.Children.toArray(children).length+1}, minmax(0, 1fr))`}} className='grid absolute w-full h-full'>
                 {getCells()}
@@ -222,17 +185,17 @@ function RootBranch({children, rootList , trains , branches , d0, d1 }: {childre
 function ChildBranch(props: any) {
 
     return (
+        <>
         <div style={{gridTemplateColumns: 'subgrid', gridColumn: props.start+'/'+(props.start+props.children.length)}} className='grid overflow-hidden rounded-full shadow-md'>
             {props.children}
         </div>
+        </>
+        
     )
 }
 
 
-export default function TrainMap({ branches , color } : { branches : Array<string>[], color: string}) {
-    const [overflow, setOverflow] = useState(0);
-    const [table, setTable] = useState(<div className='hi'></div>);
-    
+export default function TrainMap({ branches , color , branchNames } : { branches : Array<string>[], color: string, branchNames: string[]}) {
     staticColor = color;
     const trains = new LinkedTrain();
     var currentBranch = branches[0];
@@ -244,8 +207,6 @@ export default function TrainMap({ branches , color } : { branches : Array<strin
             return currentBranch
         }
     })
-
-    console.log(fBranches);
 
     for (let branch of fBranches) {
         trains.insertLastNode(branch);
@@ -263,13 +224,18 @@ export default function TrainMap({ branches , color } : { branches : Array<strin
 
         // render child branch starting on total offset distance of the contained grid
         return (
-            <ChildBranch start={offset + 1}>{buildBranch(branch, offset, index + 1, color)}</ChildBranch>
+            <ChildBranch start={offset + 1}>{_buildBranch(branch, offset, index + 1, color)}</ChildBranch>
         )
     })
 
     return (
-        <RootBranch rootList={buildBranch(fBranches[0], 0, 0, color)} trains={trains} branches={fBranches}>
+        <div className='flex flex-row w-full h-auto gap-x-2'>
+            <RootBranch rootList={_buildBranch(fBranches[0], 0, 0, color)} trains={trains} branches={fBranches} branchNames={branchNames}>
             {branchList}
-        </RootBranch>
+            </RootBranch>
+            <div className='h-full w-auto flex flex-col gap-y-3'>
+                {branchNames.map(name => <div className='w-full flex-grow flex justify-center items-center'><div style={{color: color, backgroundColor: color+"45"}} className=' bg-gray-400 rounded-full text-[10px] py-[2px] px-[10px] drop-shadow-md h-auto w-auto font-Inter'>{name}</div></div>)}
+            </div>
+        </div>
     )
 }
---
+++ pages/train/utils/mbta-fetch.tsx
@@ -0,0 +1,86 @@
+import MBTA, { MBTAResponse } from 'mbta-client';
+import { Stop , Position } from './types';
+
+const API_KEY = '7226f7ee55514c019819bac6eacdaad9';
+const mbta = new MBTA(API_KEY);
+
+
+function _compareDates(a: Date, b: Date): Date {
+    // compares two Date objects and returns the lowest of the two
+    if (a.getTime() > b.getTime()) {
+        return b
+    } else if (a.getTime() < b.getTime()) {
+        return a
+    } else {
+        return b
+    }
+}
+
+
+async function fetchStops(name: string) {
+    return await mbta.fetchStopsByRoute(name)
+        .then((response: Stop[]) => {
+            let stops: Set<string> = new Set([]);
+            response.map(response_item  => {
+                stops.add(response_item.id);
+            })
+            return stops
+        })
+}
+
+
+async function _fetchNextTrainTime(stop_id: string, direction_id: number) {
+    const handleResponse = (res?: MBTAResponse) => {
+        // filter for upcoming train times that have a valid timestamp
+        var times = res.data.map((d: any) => {
+            if (d.attributes.arrival_time != null) {
+                var timestamp = new Date(d.attributes.arrival_time);
+
+                if (timestamp.getTime() > Date.now()) {
+                    return timestamp
+                }
+            }
+        }).filter((d: any) => d != null);
+
+        // find the soonest arriving train predicted to be arriving
+        var nearestArrival = null;
+        for (var timestamp of times) {
+            if (nearestArrival != null) {
+                nearestArrival = _compareDates(timestamp, nearestArrival);
+            } else {
+                nearestArrival = timestamp;
+            }
+        }
+
+        // returns the soonest arriving train timestamp (returns 'null' if station unavailable)
+        return nearestArrival
+    }
+
+    return await mbta.fetchPredictions({stop: stop_id, sort:"arrival_time", limit:10, direction_id:direction_id}).then((res?: MBTAResponse) => handleResponse(res));
+}
+
+async function fetchStopData(ref: Position) {
+    // Make a request for next train times for the currently selected stop for both directions and assign the promises
+    const responseA = _fetchNextTrainTime(ref.name, 0);
+    const responseB = _fetchNextTrainTime(ref.name, 1);
+
+    // Make a request for the current selected stops full stop info and assign the promise
+    const responseC = mbta.fetchStops({id:ref.name});
+    
+    // Set default output to prevent type errors within other components
+    var result = {right: null, left: null, name: null};
+
+    // Once all three promises are fulfilled, assign the result
+    result = await Promise.all([responseA, responseB, responseC]).then(res => {
+        return {
+            right: res[0],
+            left: res[1],
+            name: res[2].data[0].attributes.name
+        }
+    })
+
+    // If the Promise is unable to be fulfilled, the output will be the default result
+    return result
+}
+
+export { fetchStops , fetchStopData };
