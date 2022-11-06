6ef517a28ea9152aba484140b94188fa7ce8a5b3
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2022-11-06 16:57:39 -0500

    next/link

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navigation({ color }: {color: string}) {
     const defaultColor = 'bg-['+color+']';
     const Ping = () => <span className="animate-ping bg-blue-500 w-2 h-2 absolute left-[-4px] top-[-4px] rounded-full"></span>;
@@ -21,10 +23,10 @@ export default function Navigation({ color }: {color: string}) {
             <style>{css}</style>
             <div className="subway-select w-auto h-auto grid grid-cols-4 font-Inter font-light text-md gap-2 p-4">
                 <div className="grid-label col-span-4 h-auto py-[2px] font-Inter font-medium text-lg text-center">Subway Selection</div>
-                <a className={`bg-default border-[1px] w-auto h-full hover:bg-[#DA291C] hover:text-white hover:border-transparent shadow-md rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`} href="/red">Red</a>
-                <a className={`bg-default border-[1px] w-auto h-full hover:bg-[#FA902D] hover:text-white hover:border-transparent shadow-sm rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`} href="/orange">Orange</a>
-                <a className={`bg-default border-[1px] w-auto h-full hover:bg-[#3551DE] hover:text-white hover:border-transparent shadow-sm rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`} href="/blue">Blue</a>
-                <a className={`bg-default border-[1px] w-auto h-full hover:bg-[#139C13] hover:text-white hover:border-transparent shadow-sm rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`} href="/green">Green</a>
+                <Link href="/red"><div className={`bg-default border-[1px] w-auto h-full hover:bg-[#DA291C] hover:text-white hover:border-transparent shadow-md rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`}>Red</div></Link>
+                <Link href="/orange"><div className={`bg-default border-[1px] w-auto h-full hover:bg-[#FA902D] hover:text-white hover:border-transparent shadow-sm rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`}>Orange</div></Link>
+                <Link href="/blue"><div className={`bg-default border-[1px] w-auto h-full hover:bg-[#3551DE] hover:text-white hover:border-transparent shadow-sm rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`}>Blue</div></Link>
+                <Link href="/green"><div className={`bg-default border-[1px] w-auto h-full hover:bg-[#139C13] hover:text-white hover:border-transparent shadow-sm rounded-md transition-colors flex justify-center items-center px-4 py-2 relative`}>Green</div></Link>
             </div>
         </div>
     )
